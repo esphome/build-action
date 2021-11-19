709624d1be9f3c3554cdbce74a91e655034fa650
Jesse Hills <3060199+jesserockz@users.noreply.github.com>
2021-11-19 13:10:43 +1300

    Missing =

--- rootfs/usr/local/bin/entrypoint.sh
+++ rootfs/usr/local/bin/entrypoint.sh
@@ -4,7 +4,7 @@ set -e
 
 esphome compile $1
 
-version$(esphome version | cut -d' ' -f2)
+version=$(esphome version | cut -d' ' -f2)
 echo "::set-output name=esphome-version::$version"
 
 exec /usr/local/bin/get_flash_images.py $1
